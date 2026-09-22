5c55bc1d883ddf553698e31d6cce088dbefa9a4e
Pouya Irannezhad <pouya.irannezhad1384@gmail.com>
2026-09-22 17:43:30 +0300

    feat(Models):add Post Relationship  and Fillable

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+
+#[Fillable('user_id', 'category_id', 'title', 'image', 'is_active', 'is_featured')]
 class Post extends Model
 {
-    //
+    public function category(): BelongsTo
+    {
+        return $this->BelongsTo(Category::class, 'category_id');
+    }
+
+    public function tag(): BelongsTo
+    {
+        return $this->BelongsTo(Tag::class, 'tag_id');
+    }
 }
